4fb776e8c11f1efc9beac525798ce70be8e1421c
Nora <nora.palsrud@visma.com>
2022-10-31 19:12:24 +0100

    økt buffersize for webclient

--- src/main/java/no/faggruppe/java/PokemonAPI/config/WebClientConfiguration.java
+++ src/main/java/no/faggruppe/java/PokemonAPI/config/WebClientConfiguration.java
@@ -2,12 +2,19 @@ package no.faggruppe.java.PokemonAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
     @Bean("webClient")
     public WebClient webClient() {
-        return WebClient.create();
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+        return WebClient.builder()
+                .exchangeStrategies(strategies)
+                .build();
     }
 }
